9702892267255fa05326bb243cccf660f43dc424
Christopher Trott <chris@twocentstudios.com>
2015-10-22 17:36:16 -0500

    Create l10 extension

---
+++ livestream/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  livestream
+//
+//  Created by Christopher Trott on 10/22/15.
+//  Copyright © 2015 twocentstudios. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func l10() -> String {
+        return String.localizedStringWithFormat(self)
+    }
+}
